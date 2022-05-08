d5eaaa48dd163e9b5b80ab2da154526c1df8f3f4
adxiong <260245435@qq.com>
2022-05-08 22:19:30 +0800

    update

---
+++ src/utils/track.ts
@@ -0,0 +1,47 @@
+import React, { BaseSyntheticEvent } from 'react';
+import {ReactPropTypes } from 'react';
+/*
+ * @Description: 
+ * @version: 
+ * @Author: Adxiong
+ * @Date: 2022-05-08 11:45:06
+ * @LastEditors: Adxiong
+ * @LastEditTime: 2022-05-08 12:38:33
+ */
+type OnClickEvent = (message:any) => void
+
+type OnRouterChange = (url:string) => void
+
+interface InitReactTrack {
+  onClickEvent?:OnClickEvent;
+  onRouterChange?:OnRouterChange;
+}
+
+interface ExtendReactPropTypes extends ReactPropTypes{
+  track: any
+}
+
+const propTrackWithEvent = (props: any, callback: (message: any) => void) => {
+  if(props.onClick) {
+    const originClick = props.onClick
+    props.onClick = (e: BaseSyntheticEvent)=> {
+      callback && callback(props.track)
+      originClick(e)
+    }
+  }
+  return props
+}
+
+const initTrackEvent = ( {onClickEvent, onRouterChange }: InitReactTrack) => {
+  const originReactCreateElement = React.createElement
+  
+  React.createElement = (...args: any[]) => {
+    let props = args[1]
+    if(onClickEvent && props.track) {
+      props = propTrackWithEvent(props, onClickEvent)
+    }
+    return originReactCreateElement(type, props, children)
+  }
+}
+
+export default initTrackEvent
